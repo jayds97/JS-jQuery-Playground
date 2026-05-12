8242ca693ddd37d10a0305aa7b6d4a144eab988d
Jayant Sarwale <jayantsarwale@gmail.com>
2026-05-12 23:18:23 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,47 @@
+console.log("Welcome to JS");
+console.log("Developed by Jayant Dhananjay Sarawale");
+name = "john snow";
+nval = null;
+age = 29;
+percentage = undefined;
+bval = false
+console.log(name,nval,age,percentage,bval);
+const gravity = 21.25;
+let name = "Jayant";
+let age = 24;
+
+console.log(name,age,gravity);
+let price = 100.0;
+let fullName ="jayant"
+let x;
+let y = null
+console.log(typeof price)
+console.log(typeof fullName)
+console.log(typeof x)
+console.log(typeof y)
+
+const student ={
+    fullName : "Jayant Dhananjay Sarawale",
+    age : 29,
+    isAdult :true
+};
+student['age'] = student['age'] +1
+student['fullName'] = "Shripad Pawar"
+console.log(student);
+
+const prodDetails = {
+    prodName : "Ball Pen",
+    rating:3.2,
+    isOffer:true,
+    discount : "10%"
+}
+
+console.log(prodDetails)
+
+const usrProfile ={
+    fullName: "Jayant",
+    usrName: "jayds97",
+    isFollow: false,
+    followers : 24125
+}
+console.log(usrProfile)
